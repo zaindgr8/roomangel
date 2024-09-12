234993faf133b5dfbdd51d500ba1c1195ebd767a
fazal wahab <wahabdir567@gmail.com>
2024-09-12 05:51:41 -0700

    add new links for image

--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -16,7 +16,7 @@ const Sidebar = () => {
             </Link>
           </li>
           <li>
-            <Link href="/hotel-form" className="block py-2 px-4  rounded-md">
+            <Link href="/hotel-image" className="block py-2 px-4  rounded-md">
               Hotel Images
             </Link>
           </li>
@@ -26,7 +26,7 @@ const Sidebar = () => {
             </Link>
           </li>
           <li>
-            <Link href="/room-form" className="block py-2 px-4  rounded-md">
+            <Link href="/room-image" className="block py-2 px-4  rounded-md">
               Room Images
             </Link>
           </li>
